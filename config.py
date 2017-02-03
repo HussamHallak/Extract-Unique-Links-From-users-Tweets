e6f437216918c7021b4c2a51c96588d0309ed075
HussamHallak <hhallak@cs.odu.edu>
2017-02-03 15:52:39 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+#Get your Twitter API credentials and enter them here
+consumer_key = "2oBXvFy1y4X5CU531SRZzKkGK"
+consumer_secret = "sb8tksvxRBMzhRW7TJXfjtxuEkV2n6cEZkc7RhbOue5GoGTdpX"
+access_key = "825218681252765696-vjN9uBRup9LHTlhaoD4trtbq10FJmJr"
+access_secret = "GdEADmDeddsxoyKDoco0gRuv3DqUdNGiQ6ogFwcAmjCrP"
