708b3508b7886b9077158f08066db5a506cffd81
HussamHallak <hhallak@cs.odu.edu>
2017-02-03 16:40:27 -0500

    Update make_unique.py

--- make_unique.py
+++ make_unique.py
@@ -1,7 +1,13 @@
-seen_lines = set() 
-outputfile = open("unique_links.txt", "w")
-for line in open("links.txt", "r"):
-    if line not in seen_lines:
-        outputfile.write(line)
-        seen_lines.add(line)
-outputfile.close()
+import sys
+
+if len(sys.argv) < 3:
+        print "Usage: Python make_unique.py <input_file> <output_file>"
+        print "e.g: Python make_unique.py links.txt unique_links.txt"
+else:
+        seen_lines = set()
+        outputfile = open(sys.argv[2], "w")
+        for line in open(sys.argv[1], "r"):
+                if line not in seen_lines:
+                        outputfile.write(line)
+                        seen_lines.add(line)
+        outputfile.close()
