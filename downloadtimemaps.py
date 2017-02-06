39038f1e97f72d49b52f59f1eab7657b6f6bd066
HussamHallak <hhallak@cs.odu.edu>
2017-02-06 17:28:30 -0500

    Update downloadtimemaps.py

--- downloadtimemaps.py
+++ downloadtimemaps.py
@@ -1,21 +1,24 @@
 import sys
+import os
 import urllib2
 if len(sys.argv) != 3:
         print "Usage: Python download_timemaps.py <input_file> <output_file>"
         print "e.g: Python download_timemaps.py unique_links.txt timemap.txt"
 else:
-        fh_input(sys.argv[1], 'r')
+        fh_input = open(sys.argv[1], 'r')
         i = 0
         with open(sys.argv[1]) as fp:
             for line in fp:
                 link =  "http://memgator.cs.odu.edu/timemap/link/" + line
-                response = urllib2.urlopen(link)
-                content = response.read()
-                folder_name = "/TimeMaps"
-                if not os.path.isdir(folder_name):
-                        os.makedirs(folder_name)
-                output_file_name = folder_name + sys.argv[2] + str(i)
-                fh_output = open(output_file_name, "w")
-                fh_output.write(content)
-                fh_output.close()
+                print link
+                try:
+                        response = urllib2.urlopen(link)
+                        content = response.read()
+                        output_file_name = sys.argv[2] + str(i)
+                        fh_output = open(output_file_name, "w")
+                        fh_output.write(content)
+                        fh_output.close()
+                except:
+                        print "error"
         fh_input.close()
+
