a890e5ca5b0297dc8b7ed1542632ac895ada1254
HussamHallak <hhallak@cs.odu.edu>
2017-02-06 01:57:47 -0500

    Create download_timemaps.py

---
+++ download_timemaps.py
@@ -0,0 +1,13 @@
+import sys
+
+if len(sys.argv) != 3:
+        print "Usage: Python make_unique.py <input_file> <output_file>"
+        print "e.g: Python make_unique.py unique_links.txt timemap.txt"
+else:
+        seen_lines = set()
+        outputfile = open(sys.argv[2], "w")
+        for line in open(sys.argv[1], "r"):
+                if line not in seen_lines:
+                        outputfile.write(line)
+                        seen_lines.add(line)
+outputfile.close()
