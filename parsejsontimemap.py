7acc0cfaa6d4691ffec649d2fcb15e0d3efb8299
HussamHallak <hhallak@cs.odu.edu>
2017-02-07 06:59:46 -0500

    Update and rename parse_timemap.json.py to parsejsontimemap.py

---
+++ parsejsontimemap.py
@@ -0,0 +1,19 @@
+import sys
+import json
+
+if len(sys.argv) != 3:
+    print "Usage: Python parsejsontimemap.py <input_file> <output_file>"
+    print "e.g: Python parsejsontimemap.py timemap.json timemap_report.txt"
+else:
+    fh_output = open(sys.argv[2] , 'w')
+    i = 0
+    for i in range(0,999):
+        input_file_name = sys.argv[1] + str(i)
+        if (fh_input = open(input_file_name, 'r')):
+            data = json.loads(json_data)
+            number_of_mementos = len(data['mementos'])
+            i = i + 1
+            fh_output.write(number_of_mementos)
+            fh_output.write('\n')
+            fh_input.close()
+    fh_output.close()
