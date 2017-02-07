67b104168264fb3b0f9c20ace3a1c6a6b50654b7
HussamHallak <hhallak@cs.odu.edu>
2017-02-07 16:25:14 -0500

    Update parsejsontimemap.py

--- parsejsontimemap.py
+++ parsejsontimemap.py
@@ -1,19 +1,17 @@
 import sys
 import json
-
+import os
 if len(sys.argv) != 3:
-    print "Usage: Python parsejsontimemap.py <input_file> <output_file>"
-    print "e.g: Python parsejsontimemap.py timemap.json timemap_report.txt"
+        print "Usage: Python parsejsontimemap.py <input_file> <output_file>"
+        print "e.g: Python parsejsontimemap.py timemap.json timemapreport.txt"
 else:
-    fh_output = open(sys.argv[2] , 'w')
-    i = 1
-    for i in range(0,999):
-        input_file_name = sys.argv[1] + str(i)
-        if (fh_input = open(input_file_name, 'r')):
-            data = json.loads(json_data)
-            number_of_mementos = len(data['mementos'])
-            i = i + 1
-            fh_output.write(number_of_mementos)
-            fh_output.write('\n')
-            fh_input.close()
-    fh_output.close()
+        fh_output = open(sys.argv[2] , 'w')
+        for i in range(1,446):
+                input_file_name = sys.argv[1] + str(i)
+                data_file = open(input_file_name, 'r')
+                data = json.load(data_file)
+                N = len(data['mementos']['list'])
+                N = str(N)
+                fh_output.write(N)
+                fh_output.write("\n")
+        fh_output.close()
