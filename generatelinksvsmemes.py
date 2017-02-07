f8dab959cec2fb837ec861d896656cd097ee8bae
HussamHallak <hhallak@cs.odu.edu>
2017-02-07 17:08:56 -0500

    Update and rename generatehistogramdata4timemaps.py to generatelinksvsmemes.py

---
+++ generatelinksvsmemes.py
@@ -0,0 +1,24 @@
+import sys
+from collections import Counter
+
+if len(sys.argv) != 3:
+    print "Usage: python generatelinksvsmemes.py <input_file> <output_file>"
+    print "e.g: python generatelinksvsmemes.py timemapreport.txt memesvslinks.txt"
+else:
+    fh_output = open(sys.argv[2] , 'w')
+    number_of_memes = []
+    fh_input = open(sys.argv[1], "r")
+
+    for line in fh_input:
+        line = line.strip('\n')
+        number_of_memes.append(line)
+
+    fh_output.write('Number of mementos (X)     Links Count (Y)')
+    fh_output.write('\n')
+    C = Counter(number_of_memes)
+    for k,v in C.items():
+        fh_output.write(str(k)+'                        '+str(v))
+        fh_output.write('\n')
+    fh_input.close()
+    fh_output.close()
+~
